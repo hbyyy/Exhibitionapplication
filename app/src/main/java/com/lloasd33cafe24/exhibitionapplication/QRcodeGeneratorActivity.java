6fc04f784522f85bfa2d3b1c89dd48f18d537d39
이정민 <jmlee2013@gmail.com>
2019-05-01 15:35:15 +0900

    QR코드 라이브러리 gradle에 추가. QRcodeGenerator클래스 추가 (Java, xml) * 일단 url을 editText에 입력하고 생성 버튼 클릭하는 방식으로 구현 -> 작품페이지 등록 버튼에 연결하고, editText없이 바로 변수로 받아 생성하게 수정해야 함

---
+++ app/src/main/java/com/lloasd33cafe24/exhibitionapplication/QRcodeGeneratorActivity.java
@@ -0,0 +1,48 @@
+package com.lloasd33cafe24.exhibitionapplication;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class QRcodeGeneratorActivity extends AppCompatActivity {
+    EditText text;
+    Button gen_btn;
+    ImageView image;
+    String text2Qr;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_generator);
+        text = findViewById(R.id.urlInput);
+        gen_btn = findViewById(R.id.gen_btn);
+        image = findViewById(R.id.codeImage);
+
+        gen_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text2Qr = text.getText().toString().trim();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try{
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text2Qr, BarcodeFormat.QR_CODE,200,200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    image.setImageBitmap(bitmap);
+                }
+                catch (WriterException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
+
